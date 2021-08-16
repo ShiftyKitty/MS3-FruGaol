04fc4292152db4aa6edc550c14f7c1e806e3398b
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-08-16 11:06:04 +0000

    Signup, Business Sign Up and Consumer sign up pages now created

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
 import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,26 @@ mongo = PyMongo(app)
 def get_product_offers():
     product_offers = mongo.db.product_offers.find()
     return render_template("offers.html", product_offers=product_offers)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/business_signup", methods=["GET", "POST"])
+def business_signup():
+    return render_template("business_signup.html")
+
+
+@app.route("/consumer_signup", methods=["GET", "POST"])
+def consumer_signup():
+    return render_template("consumer_signup.html")
+
+
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
+    return render_template("upload_image.html")
 
 
 if __name__ == "__main__":
