235d81ad2188664b57efc93435b04de4a7120381
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-08-18 13:05:21 +0000

    user input and image upload now saving to relevant mongodb business_users collection

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+app.config['MAX_IMAGE_FILESIZE'] = 16 * 1000 * 1000
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
+
 mongo = PyMongo(app)
 
 
@@ -31,8 +34,87 @@ def signup():
     return render_template("signup.html")
 
 
+def allowed_logo(filename):
+    if "." not in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+def allowed_logo_filesize(filesize):
+
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+
+
 @app.route("/business_signup", methods=["GET", "POST"])
 def business_signup():
+    # business logo saved to mongodb
+    # if "logo" in request.files:
+    #     logo = request.files["logo"]
+    #     mongo.save_file(logo.filename, logo)
+
+    if request.method == "POST":
+
+        existing_email = mongo.db.business_users.find_one(
+            {"business_email_address": request.form.get("business_email_address").lower()})
+
+        if existing_email:
+            flash("Email Address already in use")
+            return redirect(url_for("business_signup"))
+
+        if request.files:
+            logo = request.files["logo"]
+
+            if "filesize" in request.cookies:
+
+                if not allowed_logo_filesize(request.cookies["filesize"]):
+                    flash("Filesize exceeded maximum limit")
+                    return redirect(request.url)
+
+                logo = request.files["logo"]
+
+                if logo.filename == '':
+                    flash("No filename. Please name file and try again")
+                    return redirect(url_for("business_signup"))
+
+                if allowed_logo(logo.filename):
+                    filename = secure_filename(logo.filename)
+                    mongo.save_file(filename, logo)
+                    print("Logo saved")
+                    return redirect(url_for("business_signup"))
+                else:
+                    flash("That file extension is not permitted")
+                    return redirect(url_for("business_signup"))
+
+        business_signup = {
+            "business_name": request.form.get("business_name"),
+            "signers_name": request.form.get("sign_up_name"),
+            "industry": request.form.get("industry"),
+            "business_address_line_1": request.form.get("business_address_line_1"),
+            "business_address_line_2": request.form.get("business_address_line_2"),
+            "business_address_line_3": request.form.get("business_address_line_3"),
+            "business_contact_number": request.form.get("business_contact_number"),
+            "business_email_address": request.form.get("business_email_address"),
+            "facebook": request.form.get("facebook"),
+            "instagram": request.form.get("instagram"),
+            "twitter": request.form.get("twitter"),
+            "website_url": request.form.get("website_url"),
+            "logo_img": logo.filename,
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.business_users.insert_one(business_signup)
+
+        session["business_user"] = request.form.get("business_name")
+        flash("Business Sign Up Successful")
+
     return render_template("business_signup.html")
 
 
@@ -41,9 +123,9 @@ def consumer_signup():
     return render_template("consumer_signup.html")
 
 
-@app.route("/upload_image", methods=["GET", "POST"])
-def upload_image():
-    return render_template("upload_image.html")
+@app.route("/upload_logo", methods=["GET", "POST"])
+def upload_logo():
+    return render_template("upload_logo.html")
 
 
 if __name__ == "__main__":
